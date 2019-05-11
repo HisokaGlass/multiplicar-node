7a5027701ac4eaedbb88610e131e47b44179624d
Carlos <carlos.hernandezisc@gmail.com>
2019-05-10 22:37:33 -0500

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,43 @@
+// requireds
+const fs = require('fs');
+
+
+let listarTabla = (base, limite = 10) => {
+
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${base} * ${ i } = ${base * i }`)
+    }
+
+}
+
+
+let crearArchivo = (base, limite = 10) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${ base } no es un número`);
+            return
+        }
+
+
+        let tabla = '';
+
+        for (let i = 1; i <= limite; i++) {
+            tabla += `${base} * ${ i } = ${base * i }\n`;
+        }
+
+        const data = new Uint8Array(Buffer.from('Hello Node.js'));
+        fs.writeFile(`tablas/tabla-${ base }-al-${ limite }.txt`, tabla, (err) => {
+            if (err)
+                reject(err)
+            else
+                resolve(`tabla-${base}-al-${limite}.txt`)
+        });
+    });
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
